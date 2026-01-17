a70933ea8b7d1bfe4512dbd814ce3632113aa952
cole-451 <coleneely87@gmail.com>
2026-01-16 17:38:41 -0700

    added wander state

--- Assets/AutonomousAgent/Scripts/AutonomousAgent.cs
+++ Assets/AutonomousAgent/Scripts/AutonomousAgent.cs
@@ -1,3 +1,6 @@
+
+
+using Unity.VisualScripting;
 using UnityEngine;
 
 public class AutonomousAgent : AIAgent
@@ -6,18 +9,64 @@ public class AutonomousAgent : AIAgent
     [SerializeField] Perception seekperception;
     [SerializeField] Perception fleeperception;
 
+    [Header("Wander")]
+
+    [SerializeField] float wanderRadius = 1;
+
+    [SerializeField] float wanderDistance = 1;
+
+    [SerializeField] float wanderDisplacement = 1;
+
+
+
+    float wanderAngle = 0.0f;
+
     void Start()
     {
+        wanderAngle = Random.Range(0.00f, 100.00f);
+    }
+
+    private UnityEngine.Vector3 Wander()
+
+    {
+
+        // randomly adjust the wander angle within (+/-) displacement range 
+
+        wanderAngle += wanderAngle - wanderDisplacement - wanderDisplacement;
+
+        // calculate a point on the wander circle using the wander angle 
+
+        UnityEngine.Quaternion rotation = UnityEngine.Quaternion.AngleAxis(wanderAngle, UnityEngine.Vector3.up);
+
+        UnityEngine.Vector3 pointOnCircle = rotation * (Vector3.forward *wanderRadius);
+
+        // project the wander circle in front of the agent 
+
+        UnityEngine.Vector3 circleCenter =  movement.Direction * wanderDistance;
+
+        // steer toward the target point (circle center + point on circle) 
+
+        UnityEngine.Vector3 force = GetSteeringForce(circleCenter+ pointOnCircle );
+
+
+
+        return force;
 
     }
 
     void Update()
     {
+        // store if target found, used for wander if no target 
+
+        bool hasTarget = false;
+
+
         if (seekperception != null)
         {
         var gameObjects = seekperception.GetGameObjects();
         if (gameObjects.Length > 0)
         {
+                hasTarget = true;
             Vector3 force = Seek(gameObjects[0]);
             movement.ApplyForce(force);
         }
@@ -27,15 +76,23 @@ public class AutonomousAgent : AIAgent
        var gameObjects = fleeperception.GetGameObjects();
         if (gameObjects.Length > 0)
         {
+                hasTarget |= true;
             Vector3 force = Flee(gameObjects[0]);
             movement.ApplyForce(force);
         }
         }
-        //foreach (var go in gameObjects) {
-        //    Debug.DrawLine(transform.transform.position, go.transform.position, Color.orange);
-        //}
+        // if no target then wander 
 
-       // movement.ApplyForce(transform.forward);
+        if (!hasTarget)
+
+        {
+
+            Vector3 force = Wander();
+
+            movement.ApplyForce(force);
+        
+        }
+
 
         transform.position = Utilities.Wrap(transform.position, new Vector3(-15, -15, -15), new Vector3 (15,15,15));
         if (movement.Velocity.sqrMagnitude > 0)
